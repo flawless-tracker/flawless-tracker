15fd5ad4f8d740dd66cee9b4f727e7b5f30a81c2
Viacheslav Okolitiy <justforgit@gmail.com>
2012-10-01 02:33:38 +0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlawlessTracker::Application.config.secret_token = 'aa5b0b34b69d1b5c5274acffda39a4a244b5cf03be8c8ed767f7e4bb04fb90f1b771b7be6af1b1d62042a060d62de7c6f57832eb3096760b6c92fedc86bc392e'
